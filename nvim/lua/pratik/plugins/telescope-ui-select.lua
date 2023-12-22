ef98224ab87ed156a63a52eea8fd534dea928b51
Pratik Thapa <pratik@tldrnotes.net>
2023-12-21 21:50:12 -0500

    telescope ui select for code actions

---
+++ nvim/lua/pratik/plugins/telescope-ui-select.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		-- for nice code actions select window
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+}
