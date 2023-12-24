c44764ff3836631e8e6c59c36a9e1711ea4de0f3
Pratik Thapa <pratik@tldrnotes.net>
2023-12-24 10:23:06 -0500

    disabling custom lualine theme

--- nvim/lua/pratik/plugins/lualine.lua
+++ nvim/lua/pratik/plugins/lualine.lua
@@ -17,44 +17,45 @@ return {
 			inactive_fg = "#62666e",
 		}
 
-		local my_lualine_theme = {
-			normal = {
-				a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			insert = {
-				a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			visual = {
-				a = { bg = colors.greenish, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			command = {
-				a = { bg = colors.orange, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			replace = {
-				a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-				b = { bg = colors.bg, fg = colors.fg },
-				c = { bg = colors.bg, fg = colors.fg },
-			},
-			inactive = {
-				a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-				b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-				c = { bg = colors.inactive_fg, fg = colors.semilightgray },
-			},
-		}
+		-- local my_lualine_theme = {
+		-- 	normal = {
+		-- 		a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
+		-- 		b = { bg = colors.bg, fg = colors.fg },
+		-- 		c = { bg = colors.bg, fg = colors.fg },
+		-- 	},
+		-- 	insert = {
+		-- 		a = { bg = colors.green, fg = colors.bg, gui = "bold" },
+		-- 		b = { bg = colors.bg, fg = colors.fg },
+		-- 		c = { bg = colors.bg, fg = colors.fg },
+		-- 	},
+		-- 	visual = {
+		-- 		a = { bg = colors.greenish, fg = colors.bg, gui = "bold" },
+		-- 		b = { bg = colors.bg, fg = colors.fg },
+		-- 		c = { bg = colors.bg, fg = colors.fg },
+		-- 	},
+		-- 	command = {
+		-- 		a = { bg = colors.orange, fg = colors.bg, gui = "bold" },
+		-- 		b = { bg = colors.bg, fg = colors.fg },
+		-- 		c = { bg = colors.bg, fg = colors.fg },
+		-- 	},
+		-- 	replace = {
+		-- 		a = { bg = colors.red, fg = colors.bg, gui = "bold" },
+		-- 		b = { bg = colors.bg, fg = colors.fg },
+		-- 		c = { bg = colors.bg, fg = colors.fg },
+		-- 	},
+		-- 	inactive = {
+		-- 		a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
+		-- 		b = { bg = colors.inactive_bg, fg = colors.semilightgray },
+		-- 		c = { bg = colors.inactive_fg, fg = colors.semilightgray },
+		-- 	},
+		-- }
 
 		-- configure lualine with modified theme
 		lualine.setup({
-			options = {
-				theme = my_lualine_theme,
-			},
+			-- custom lualine theme
+			-- options = {
+			-- 	theme = my_lualine_theme,
+			-- },
 			sections = {
 				lualine_x = {
 					{
