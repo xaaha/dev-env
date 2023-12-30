67204f6f56191156e9d9a28a9306b68f9d833882
Pratik Thapa <pratik@tldrnotes.net>
2023-12-30 14:05:53 -0500

    noice changes

--- nvim/lua/pratik/plugins/colorscheme.lua
+++ nvim/lua/pratik/plugins/colorscheme.lua
@@ -2,7 +2,6 @@ return {
 	{
 		"catppuccin/nvim",
 		as = "catppuccin",
-		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
 			-- configure the colorscheme here
 			require("catppuccin").setup({
---
+++ nvim/lua/pratik/plugins/noice.lua
@@ -0,0 +1,44 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add any options here
+	},
+	dependencies = {
+		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+		"MunifTanjim/nui.nvim",
+		-- `nvim-notify` feels obnixious to me
+		-- "rcarriga/nvim-notify",
+	},
+	config = function()
+		require("noice").setup({
+			views = {
+				cmdline_popup = {
+					position = {
+						row = "95%", -- or row number: 5
+						col = "5%",
+					},
+					size = {
+						width = 40, -- "auto" is 60
+						height = "auto",
+					},
+				},
+			},
+			cmdline = {
+				format = {
+					search_down = {
+						icon = "🔍⌄",
+					},
+					search_up = {
+						icon = "🔍⌃",
+					},
+				},
+			},
+			lsp = {
+				signature = {
+					enabled = false, -- using lsp-signature instead, which has a nice panda
+				},
+			},
+		})
+	end,
+}
