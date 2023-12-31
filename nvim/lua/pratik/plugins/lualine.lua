c936e7d181db976bab4ac9c695b26b9da321e735
Pratik Thapa <pratik@tldrnotes.net>
2023-12-31 12:46:05 -0500

    delete lualine theme

--- nvim/lua/pratik/plugins/lualine.lua
+++ nvim/lua/pratik/plugins/lualine.lua
@@ -4,64 +4,13 @@ return {
 	config = function()
 		local lualine = require("lualine")
 		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-
-		local colors = {
-			blue = "#65D1FF",
-			green = "#06d6a0",
-			greenish = "#87bba2", -- visual mode
-			orange = "#fb8500",
-			red = "#FF4A4A",
-			fg = "#c3ccdc",
-			bg = "#112638",
-			inactive_bg = "#2c3043",
-			inactive_fg = "#62666e",
-		}
-
-		-- local my_lualine_theme = {
-		-- 	normal = {
-		-- 		a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-		-- 		b = { bg = colors.bg, fg = colors.fg },
-		-- 		c = { bg = colors.bg, fg = colors.fg },
-		-- 	},
-		-- 	insert = {
-		-- 		a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-		-- 		b = { bg = colors.bg, fg = colors.fg },
-		-- 		c = { bg = colors.bg, fg = colors.fg },
-		-- 	},
-		-- 	visual = {
-		-- 		a = { bg = colors.greenish, fg = colors.bg, gui = "bold" },
-		-- 		b = { bg = colors.bg, fg = colors.fg },
-		-- 		c = { bg = colors.bg, fg = colors.fg },
-		-- 	},
-		-- 	command = {
-		-- 		a = { bg = colors.orange, fg = colors.bg, gui = "bold" },
-		-- 		b = { bg = colors.bg, fg = colors.fg },
-		-- 		c = { bg = colors.bg, fg = colors.fg },
-		-- 	},
-		-- 	replace = {
-		-- 		a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-		-- 		b = { bg = colors.bg, fg = colors.fg },
-		-- 		c = { bg = colors.bg, fg = colors.fg },
-		-- 	},
-		-- 	inactive = {
-		-- 		a = { bg = colors.inactive_bg, fg = colors.semilightgray, gui = "bold" },
-		-- 		b = { bg = colors.inactive_bg, fg = colors.semilightgray },
-		-- 		c = { bg = colors.inactive_fg, fg = colors.semilightgray },
-		-- 	},
-		-- }
-
-		-- configure lualine with modified theme
 		lualine.setup({
-			-- custom lualine theme
-			-- options = {
-			-- 	theme = my_lualine_theme,
-			-- },
 			sections = {
 				lualine_x = {
 					{
 						lazy_status.updates,
 						cond = lazy_status.has_updates,
-						color = { fg = colors.fg },
+						color = { fg = "#c3ccdc" },
 					},
 					{ "encoding" },
 					{ "fileformat" },
