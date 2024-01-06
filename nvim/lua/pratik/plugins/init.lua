68a90df34b23f4790b6bcbc57363445bd5d4533b
Pratik Thapa <pratik@tldrnotes.net>
2024-01-06 13:45:43 -0500

    what did I do? 🤦

--- nvim/lua/pratik/plugins/init.lua
+++ nvim/lua/pratik/plugins/init.lua
@@ -1,6 +1,8 @@
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 
+	"christoomey/vim-tmux-navigator", -- moving between split windows
+
 	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 
 	"RRethy/vim-illuminate", -- highlight other uses of the word under the cursor
