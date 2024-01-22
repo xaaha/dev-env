2a3a8b9224afef649b4cc3be78aa481c6b849e40
Pratik Thapa <pratik.thapa@guildeducation.com>
2024-01-22 12:04:35 -0500

    nvim tree and updates

--- nvim/lua/pratik/plugins/nvim-tree.lua
+++ nvim/lua/pratik/plugins/nvim-tree.lua
@@ -1,5 +1,7 @@
 return {
 	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local nvimtree = require("nvim-tree")
