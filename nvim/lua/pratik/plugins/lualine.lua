9cb476c4d89bf409f35b461c6102a0e04b3d619e
Pratik Thapa <pratik.thapa@guildeducation.com>
2024-02-05 13:21:32 -0500

    update and removed Lazy Status
    Regularly Looking at things I need to update is driving me crazy

--- nvim/lua/pratik/plugins/lualine.lua
+++ nvim/lua/pratik/plugins/lualine.lua
@@ -3,16 +3,16 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		local lualine = require("lualine")
-		local lazy_status = require("lazy.status") -- to configure lazy pending updates count
+		-- local lazy_status = require("lazy.status") -- to configure lazy pending updates count
 		lualine.setup({
 			options = { section_separators = "", component_separators = "" }, -- comment out to enable default ">" section and component section separator
 			sections = {
 				lualine_x = {
-					{
-						lazy_status.updates,
-						cond = lazy_status.has_updates,
-						color = { fg = "#c3ccdc" },
-					},
+					-- {
+					-- 	lazy_status.updates,
+					-- 	cond = lazy_status.has_updates,
+					-- 	color = { fg = "#c3ccdc" },
+					-- },
 					{ "encoding" },
 					{ "fileformat" },
 					{ "filetype" },
