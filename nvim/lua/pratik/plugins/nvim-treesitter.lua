a3deddabe1d647ef475507cfb5337e3f129fcfd9
Pratik Thapa <pratik.thapa@guildeducation.com>
2024-02-07 12:00:22 -0500

    fixing treesitter

--- nvim/lua/pratik/plugins/nvim-treesitter.lua
+++ nvim/lua/pratik/plugins/nvim-treesitter.lua
@@ -1,131 +1,109 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	dependencies = {
-		{ "JoosepAlviste/nvim-ts-context-commentstring", commit = "6c30f3c8915d7b31c3decdfe6c7672432da1809d" },
-		"nvim-treesitter/nvim-treesitter-textobjects",
-		-- HACK: remove when https://github.com/windwp/nvim-ts-autotag/issues/125 closed.
-		{ "windwp/nvim-ts-autotag", opts = { enable_close_on_slash = false } },
-	},
-	event = { "BufReadPre", "BufNewFile" },
-	cmd = {
-		"TSBufDisable",
-		"TSBufEnable",
-		"TSBufToggle",
-		"TSDisable",
-		"TSEnable",
-		"TSToggle",
-		"TSInstall",
-		"TSInstallInfo",
-		"TSInstallSync",
-		"TSModuleInfo",
-		"TSUninstall",
-		"TSUpdate",
-		"TSUpdateSync",
-	},
+	version = false, -- last release is way too old and doesn't work on Windows
 	build = ":TSUpdate",
+	event = { "BufReadPre", "BufNewFile" },
 	init = function(plugin)
 		-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
 		-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
 		-- no longer trigger the **nvim-treeitter** module to be loaded in time.
 		-- Luckily, the only thins that those plugins need are the custom queries, which we make available
 		-- during startup.
-		-- CODE FROM LazyVim (thanks folke!) https://github.com/LazyVim/LazyVim/commit/1e1b68d633d4bd4faa912ba5f49ab6b8601dc0c9
 		require("lazy.core.loader").add_to_rtp(plugin)
 		require("nvim-treesitter.query_predicates")
 	end,
-	opts = function()
-		return {
-			autotag = { enable = true },
-			context_commentstring = { enable = true, enable_autocmd = false },
-			-- HACK: force install of shipped neovim parsers since TSUpdate doesn't correctly update them
-			ensure_installed = {
-				"json",
-				"javascript",
-				"typescript",
-				"tsx",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"query",
-				"python",
-				"go",
-				--"svelte",
+	dependencies = {
+		{
+			"nvim-treesitter/nvim-treesitter-textobjects",
+			config = function()
+				-- When in diff mode, we want to use the default
+				-- vim text objects c & C instead of the treesitter ones.
+				local move = require("nvim-treesitter.textobjects.move") ---@type table<string,fun(...)>
+				local configs = require("nvim-treesitter.configs")
+				for name, fn in pairs(move) do
+					if name:find("goto") == 1 then
+						move[name] = function(q, ...)
+							if vim.wo.diff then
+								local config = configs.get_module("textobjects.move")[name] ---@type table<string,string>
+								for key, query in pairs(config or {}) do
+									if q == query and key:find("[%]%[][cC]") then
+										vim.cmd("normal! " .. key)
+										return
+									end
+								end
+							end
+							return fn(q, ...)
+						end
+					end
+				end
+			end,
+		},
+	},
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	keys = {
+		{ "<c-space>", desc = "Increment selection" },
+		{ "<bs>", desc = "Decrement selection", mode = "x" },
+	},
+	opts = {
+		highlight = { enable = true },
+		indent = { enable = true },
+		ensure_installed = {
+			"bash",
+			"c",
+			"css",
+			"diff",
+			"go",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"jsonc",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"python",
+			"query",
+			"regex",
+			"toml",
+			"tsx",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		},
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<C-space>",
+				node_incremental = "<C-space>",
+				scope_incremental = false,
+				node_decremental = "<bs>",
 			},
-			highlight = {
+		},
+		textobjects = {
+			move = {
 				enable = true,
-				use_languagetree = true,
-				disable = function(_, bufnr)
-					return vim.b[bufnr].large_buf
-				end,
+				goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
+				goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
+				goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
+				goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
 			},
-			incremental_selection = { enable = true },
-			indent = { enable = true },
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["ak"] = { query = "@block.outer", desc = "around block" },
-						["ik"] = { query = "@block.inner", desc = "inside block" },
-						["ac"] = { query = "@class.outer", desc = "around class" },
-						["ic"] = { query = "@class.inner", desc = "inside class" },
-						["a?"] = { query = "@conditional.outer", desc = "around conditional" },
-						["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
-						["af"] = { query = "@function.outer", desc = "around function " },
-						["if"] = { query = "@function.inner", desc = "inside function " },
-						["al"] = { query = "@loop.outer", desc = "around loop" },
-						["il"] = { query = "@loop.inner", desc = "inside loop" },
-						["aa"] = { query = "@parameter.outer", desc = "around argument" },
-						["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-					},
-				},
-				move = {
-					enable = true,
-					set_jumps = true,
-					goto_next_start = {
-						["]k"] = { query = "@block.outer", desc = "Next block start" },
-						["]f"] = { query = "@function.outer", desc = "Next function start" },
-						["]a"] = { query = "@parameter.inner", desc = "Next argument start" },
-					},
-					goto_next_end = {
-						["]K"] = { query = "@block.outer", desc = "Next block end" },
-						["]F"] = { query = "@function.outer", desc = "Next function end" },
-						["]A"] = { query = "@parameter.inner", desc = "Next argument end" },
-					},
-					goto_previous_start = {
-						["[k"] = { query = "@block.outer", desc = "Previous block start" },
-						["[f"] = { query = "@function.outer", desc = "Previous function start" },
-						["[a"] = { query = "@parameter.inner", desc = "Previous argument start" },
-					},
-					goto_previous_end = {
-						["[K"] = { query = "@block.outer", desc = "Previous block end" },
-						["[F"] = { query = "@function.outer", desc = "Previous function end" },
-						["[A"] = { query = "@parameter.inner", desc = "Previous argument end" },
-					},
-				},
-				swap = {
-					enable = true,
-					swap_next = {
-						[">K"] = { query = "@block.outer", desc = "Swap next block" },
-						[">F"] = { query = "@function.outer", desc = "Swap next function" },
-						[">A"] = { query = "@parameter.inner", desc = "Swap next argument" },
-					},
-					swap_previous = {
-						["<K"] = { query = "@block.outer", desc = "Swap previous block" },
-						["<F"] = { query = "@function.outer", desc = "Swap previous function" },
-						["<A"] = { query = "@parameter.inner", desc = "Swap previous argument" },
-					},
-				},
-			},
-		}
+		},
+	},
+	config = function(_, opts)
+		if type(opts.ensure_installed) == "table" then
+			---@type table<string, boolean>
+			local added = {}
+			opts.ensure_installed = vim.tbl_filter(function(lang)
+				if added[lang] then
+					return false
+				end
+				added[lang] = true
+				return true
+			end, opts.ensure_installed)
+		end
+		require("nvim-treesitter.configs").setup(opts)
 	end,
-	config = require("nvim-treesitter.configs"),
 }
