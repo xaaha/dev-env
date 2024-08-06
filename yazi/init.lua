fbf54a2432069d95a7cf1fa4eabc57d6ecfe6d7d
Pratik Thapa <pratik.philos@live.com>
2024-08-06 09:16:20 -0400

    added a plugin

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,10 +1,4 @@
--- hide the status bar
-function Status:render()
-	return {}
-end
-
-local old_manager_render = Manager.render
-function Manager:render(area)
-	return old_manager_render(self, ui.Rect({ x = area.x, y = area.y, w = area.w, h = area.h + 1 }))
-end
---
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+})
