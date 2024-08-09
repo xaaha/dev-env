33874f6db4725a70346e84a07e613335b93d7b81
Pratik Thapa <pratik.philos@live.com>
2024-08-09 08:19:38 -0400

    fixed outdated cmds

--- nvim/lua/pratik/plugins/alpha-nvim.lua
+++ nvim/lua/pratik/plugins/alpha-nvim.lua
@@ -22,10 +22,14 @@ return {
 
 		-- Set menu
 		dashboard.section.buttons.val = {
+			dashboard.button(
+				"Ctrl P",
+				"󰱼 > Find File",
+				"<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>"
+			),
+			dashboard.button("SPC fg", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+			dashboard.button("Ctrl n", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
 			dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-			dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-			dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-			dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
 			dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
 		}
 
