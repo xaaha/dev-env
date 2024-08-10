524caeabc22caf6a935696c9e1cbefa3c26a4e24
Pratik Thapa <pratik.philos@live.com>
2024-08-10 08:12:44 -0400

    changing theme to rose-pine-moon

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,8 +9,8 @@ return {
 	-- font = wezterm.font_with_fallback({ "Hack Nerd Font", { family = "JetBrainsMono Nerd Font", scale = 0.75 } }),
 	use_cap_height_to_scale_fallback_fonts = true,
 	--colorscheme
-	colors = xaaha.XAAHA_BLUE,
-	-- color_scheme = "Mariana",
+	-- colors = xaaha.XAAHA_BLUE,
+	color_scheme = "rose-pine-moon",
 	-- window management
 	window_padding = {
 		left = 5,
