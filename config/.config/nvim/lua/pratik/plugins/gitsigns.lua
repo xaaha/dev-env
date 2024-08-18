7fdb331cc81ddc2e62a2def4c2f0a897f6e15633
Pratik Thapa <pratik.philos@live.com>
2024-08-18 17:04:45 -0400

    fixed the keymap for gitsigns

--- config/.config/nvim/lua/pratik/plugins/gitsigns.lua
+++ config/.config/nvim/lua/pratik/plugins/gitsigns.lua
@@ -6,7 +6,8 @@ return {
 		require("gitsigns").setup({
 			vim.keymap.set("n", "<leader>G", ":Gitsigns<CR>", { desc = "Toggle Gitsigns selector" }),
 			vim.keymap.set("n", "<leader>hl", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview hunk in line" }),
-			vim.keymap.set("n", "<leader>hr", ":Gitsigns diffthis<CR>", { desc = "Reset hunk" }),
+			vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" }),
+			vim.keymap.set("n", "<leader>hd", ":Gitsigns diffthis<CR>", { desc = "Diff this page" }),
 		})
 	end,
 }
