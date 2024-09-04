bf671e290a6df085882004fe54a34f8e395048b5
Pratik Thapa <pratik.thapa@guildeducation.com>
2024-09-04 09:57:40 -0400

    added keymaps for gitsigns

--- nvim/.config/nvim/lua/pratik/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/pratik/plugins/gitsigns.lua
@@ -8,6 +8,8 @@ return {
 			vim.keymap.set("n", "<leader>hl", ":Gitsigns preview_hunk_inline<CR>", { desc = "Preview hunk in line" }),
 			vim.keymap.set("n", "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Reset hunk" }),
 			vim.keymap.set("n", "<leader>hd", ":Gitsigns diffthis<CR>", { desc = "Diff this page" }),
+			vim.keymap.set("n", "<leader>hj", ":Gitsigns next_hunk<CR>", { desc = "Diff this page" }),
+			vim.keymap.set("n", "<leader>hb", ":Gitsigns blame_line<CR>", { desc = "Diff this page" }),
 		})
 	end,
 }
