ff50f89edc3b585fab4e51077833caef93d0c14c
Pratik Thapa <pratik.thapa@guildeducation.com>
2024-10-07 10:55:06 -0400

    added vim icon in lualine_a

--- nvim/.config/nvim/lua/pratik/plugins/lualine.lua
+++ nvim/.config/nvim/lua/pratik/plugins/lualine.lua
@@ -10,6 +10,12 @@ return {
 				section_separators = { left = "", right = "" },
 			}, -- comment out to enable default ">" section and component section separator
 			sections = {
+				lualine_a = { {
+					"mode",
+					fmt = function(str)
+						return " " .. str
+					end,
+				} },
 				lualine_b = { "branch", "diagnostics" },
 				lualine_c = { { "filename", path = 1 } }, -- 0 for file name only, 1 for relative file path
 				lualine_y = { "searchcount" },
