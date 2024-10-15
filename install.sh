98e1651ffdb6caefe0f18795a192b646488a5436
Pratik Thapa <pratik.thapa@guildeducation.com>
2024-10-15 14:30:02 -0400

    changed back

--- install.sh
+++ install.sh
@@ -22,7 +22,7 @@ if ! command -v brew &> /dev/null
 then
     echo "Homebrew not found, installing..."
 
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh) | bash
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo "Homebrew installed."
 
     # Add Homebrew to PATH
