253ac652da14123b4d4f4a8e5e997317b0b62085
pratik thapa <pratikthapa@ml2xvyvxp7.mynetworksettings.com>
2024-11-13 10:16:54 -0500

    removing indent blankline

--- nvim/.config/nvim/lua/pratik/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/pratik/plugins/indent-blankline.lua
@@ -1,7 +1,15 @@
 return {
+	-- {
+	-- 	"lukas-reineke/indent-blankline.nvim",
+	-- 	main = "ibl",
+	-- 	opts = {},
+	-- },
 	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {},
+		"nvimdev/indentmini.nvim",
+		config = function()
+			require("indentmini").setup({
+				only_current = true, -- Highlight only the current scope
+			})
+		end,
 	},
 }
