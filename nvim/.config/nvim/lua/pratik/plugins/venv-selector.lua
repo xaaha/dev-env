f2094780e7a8d304d973e59fcf25a02e0f250748
pratik thapa <pratikthapa@ml2xvyvxp7.mynetworksettings.com>
2024-11-13 17:20:40 -0500

    removing the venv selector

--- nvim/.config/nvim/lua/pratik/plugins/venv-selector.lua
+++ nvim/.config/nvim/lua/pratik/plugins/venv-selector.lua
@@ -1,19 +1,20 @@
+-- only need this while using python, disabling this for now
 return {
-	"linux-cultist/venv-selector.nvim",
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"mfussenegger/nvim-dap",
-		"mfussenegger/nvim-dap-python", --optional
-		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-	},
-	lazy = false,
-	branch = "regexp", -- This is the regexp branch, use this for the new version
-	config = function()
-		require("venv-selector").setup()
-	end,
-	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
-	keys = {
-		{ "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Open venv for python" },
-		{ "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "Retrieve venv from cache for python" },
-	},
+	-- "linux-cultist/venv-selector.nvim",
+	-- dependencies = {
+	-- 	"neovim/nvim-lspconfig",
+	-- 	"mfussenegger/nvim-dap",
+	-- 	"mfussenegger/nvim-dap-python", --optional
+	-- 	{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+	-- },
+	-- lazy = false,
+	-- branch = "regexp", -- This is the regexp branch, use this for the new version
+	-- config = function()
+	-- 	require("venv-selector").setup()
+	-- end,
+	-- event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+	-- keys = {
+	-- 	{ "<leader>vs", "<cmd>VenvSelect<cr>", desc = "Open venv for python" },
+	-- 	{ "<leader>vc", "<cmd>VenvSelectCached<cr>", desc = "Retrieve venv from cache for python" },
+	-- },
 }
