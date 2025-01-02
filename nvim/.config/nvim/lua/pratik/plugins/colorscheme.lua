636a40a75406af618f11bf914b9689d80f4e6c2b
Pratik Thapa <pratik.philos@live.com>
2025-01-01 22:32:42 -0500

    colorscheme changes

--- nvim/.config/nvim/lua/pratik/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/pratik/plugins/colorscheme.lua
@@ -46,37 +46,7 @@ return {
 		end,
 	},
 	{ "rose-pine/neovim", name = "rose-pine" },
-	{
-		"ellisonleao/gruvbox.nvim",
-		config = function()
-			require("gruvbox").setup({
-				terminal_colors = true, -- add neovim terminal colors
-				undercurl = true,
-				underline = true,
-				bold = true,
-				italic = {
-					strings = true,
-					emphasis = true,
-					comments = true,
-					operators = false,
-					folds = true,
-				},
-				strikethrough = true,
-				invert_selection = false,
-				invert_signs = false,
-				invert_tabline = false,
-				invert_intend_guides = false,
-				inverse = true, -- invert background for search, diffs, statuslines and errors
-				contrast = "hard", -- can be "hard", "soft" or empty string
-				palette_overrides = {},
-				overrides = {},
-				dim_inactive = true,
-				transparent_mode = false,
-			})
-			-- vim.cmd("colorscheme gruvbox")
-			vim.opt.background = "dark"
-		end,
-	},
+	{ "savq/melange-nvim" },
 	{
 		"neanias/everforest-nvim",
 		config = function()
