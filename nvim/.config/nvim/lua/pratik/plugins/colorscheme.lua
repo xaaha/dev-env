a625b1a3e14e381ade729bcc5b4a530c945d7090
xaaha <pratik.philos@yahoo.com>
2025-01-02 21:59:35 -0500

    cool

--- nvim/.config/nvim/lua/pratik/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/pratik/plugins/colorscheme.lua
@@ -49,7 +49,7 @@ return {
 		priority = 1000,
 		config = function()
 			require("onedark").setup({
-				style = "warm", -- Set the style to "dark"
+				style = "cool", -- Set the style
 				toggle_style_list = { "dark", "darker", "cool", "deep", "warm", "warmer", "light" },
 			})
 			require("onedark").load() -- Ensure the theme is loaded
