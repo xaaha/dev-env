9a735406e4ca9300c1a990ad28baff3c25e2de75
Pratik Thapa <pratik.philos@live.com>
2025-01-04 10:00:02 -0500

    updates and fixed the tmux navigation issue

--- nvim/.config/nvim/lua/pratik/plugins/init.lua
+++ nvim/.config/nvim/lua/pratik/plugins/init.lua
@@ -1,8 +1,6 @@
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 
-	"christoomey/vim-tmux-navigator", -- moving between split windows
-
 	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 
 	"RRethy/vim-illuminate", -- highlight other uses of the word under the cursor
@@ -14,5 +12,23 @@ return {
 	{
 		"stevearc/dressing.nvim", -- changes the default way of renaming and other ui stuff
 		event = "VeryLazy",
+	},
+	{
+		"christoomey/vim-tmux-navigator", -- moving between split windows
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+			"TmuxNavigatorProcessList",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
 	},
 }
