a56b596aa22ed4c1f481f4e7af273a0ac92ac98e
Pratik Thapa <pratik.philos@live.com>
2025-01-11 13:30:19 -0500

    zshrc changes

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,17 +1,3 @@
-function brew() {
-  if [[ $1 == "add" ]]; then
-    # Remove the first argument ("add")
-    shift
-    # Install the package
-    command brew install "$@"
-    # Update the global Brewfile
-    command brew bundle dump --global --force
-  else
-    # Call the original brew command with all original arguments
-    command brew "$@"
-  fi
-}
-
 function jwtd () {
   local input="${1:-}"
   if [ -z "$input" ]; then
