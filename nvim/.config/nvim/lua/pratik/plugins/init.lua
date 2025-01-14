3b8d5a4dd2f653ae3e3270094e51079e7d11ed24
Pratik Thapa <pratik.philos@live.com>
2025-01-13 20:14:01 -0500

    remove un-used plugins and updates

--- nvim/.config/nvim/lua/pratik/plugins/init.lua
+++ nvim/.config/nvim/lua/pratik/plugins/init.lua
@@ -1,8 +1,6 @@
 return {
 	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 
-	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
-
 	"RRethy/vim-illuminate", -- highlight other uses of the word under the cursor
 
 	"preservim/vimux", -- vim test requires this
