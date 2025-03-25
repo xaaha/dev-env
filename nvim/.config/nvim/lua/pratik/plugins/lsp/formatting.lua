948e63661ca73effde7457ac34c58eafd3a76300
xaaha <pratik.philos@yahoo.com>
2025-03-25 16:23:56 -0400

    add support for graphql with biome

--- nvim/.config/nvim/lua/pratik/plugins/lsp/formatting.lua
+++ nvim/.config/nvim/lua/pratik/plugins/lsp/formatting.lua
@@ -16,7 +16,7 @@ return {
 				json = { "biome", "prettier" },
 				yaml = { "yq", "prettier" },
 				markdown = { "prettier" },
-				graphql = { "prettier" },
+				graphql = { "prettier", "biome" },
 				lua = { "stylua" },
 				python = { "isort", "black" },
 				go = { "gofumpt", "golines", "goimports" },
