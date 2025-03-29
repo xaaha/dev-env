523db619fc257bea55c1e7392115eeae00c0766c
xaaha <pratik.philos@yahoo.com>
2025-03-29 10:50:22 -0400

    remove un-necessary defaults

--- nvim/.config/nvim/lua/pratik/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/pratik/plugins/neo-tree.lua
@@ -19,36 +19,11 @@ return {
 	end,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-		{
-			"s1n7ax/nvim-window-picker",
-			version = "2.*",
-			config = function()
-				require("window-picker").setup({
-					filter_rules = {
-						include_current_win = false,
-						autoselect_one = true,
-						-- filter using buffer options
-						bo = {
-							-- if the file type is one of following, the window will be ignored
-							filetype = { "neo-tree", "neo-tree-popup", "notify" },
-							-- if the buffer type is one of following, the window will be ignored
-							buftype = { "terminal", "quickfix" },
-						},
-					},
-				})
-			end,
-		},
+		-- "3rd/image.nvim", -- Optional image support in Preview mode
 	},
 	config = function()
-		-- If you want icons for diagnostic errors, you'll need to define them somewhere:
-		vim.fn.sign_define("DiagnosticSignError", { text = " ", texthl = "DiagnosticSignError" })
-		vim.fn.sign_define("DiagnosticSignWarn", { text = " ", texthl = "DiagnosticSignWarn" })
-		vim.fn.sign_define("DiagnosticSignInfo", { text = " ", texthl = "DiagnosticSignInfo" })
-		vim.fn.sign_define("DiagnosticSignHint", { text = "󰌵", texthl = "DiagnosticSignHint" })
-
 		require("neo-tree").setup({
 			event_handlers = {
 
@@ -66,91 +41,15 @@ return {
 			popup_border_style = "rounded",
 			enable_git_status = true,
 			enable_diagnostics = true,
-			open_files_do_not_replace_types = { "terminal", "trouble", "qf" }, -- when opening files, do not use windows containing these filetypes or buftypes
-			sort_case_insensitive = false, -- used when sorting files and directories in the tree
-			sort_function = nil, -- use a custom function for sorting files and directories in the tree
-			-- sort_function = function (a,b)
-			--       if a.type == b.type then
-			--           return a.path > b.path
-			--       else
-			--           return a.type > b.type
-			--       end
-			--   end , -- this sorts files and directories descendantly
-			default_component_configs = {
-				container = {
-					enable_character_fade = true,
-				},
-				indent = {
-					indent_size = 2,
-					padding = 1, -- extra padding on left hand side
-					-- indent guides
-					with_markers = true,
-					indent_marker = "│",
-					last_indent_marker = "└",
-					highlight = "NeoTreeIndentMarker",
-					-- expander config, needed for nesting files
-					with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-					expander_collapsed = "",
-					expander_expanded = "",
-					expander_highlight = "NeoTreeExpander",
-				},
-				icon = {
-					folder_closed = "",
-					folder_open = "",
-					folder_empty = "󰜌",
-					-- The next two settings are only a fallback, if you use nvim-web-devicons and configure default icons there
-					-- then these will never be used.
-					default = "*",
-					highlight = "NeoTreeFileIcon",
-				},
-				modified = {
-					symbol = "[+]",
-					highlight = "NeoTreeModified",
-				},
-				name = {
-					trailing_slash = false,
-					use_git_status_colors = true,
-					highlight = "NeoTreeFileName",
-				},
-				git_status = {
-					symbols = {
-						added = "+",
-						modified = "",
-						deleted = "",
-						renamed = "➜",
-						-- Status type
-						untracked = "u",
-						ignored = "◌",
-						unstaged = "✗",
-						staged = "✓",
-						conflict = "",
-					},
-				},
-				-- If you don't want to use these columns, you can set `enabled = false` for each of them individually
-				file_size = {
-					enabled = true,
-					required_width = 64, -- min width of window required to show this column
-				},
-				type = {
-					enabled = true,
-					required_width = 122, -- min width of window required to show this column
-				},
-				last_modified = {
-					enabled = true,
-					required_width = 88, -- min width of window required to show this column
-				},
-				created = {
-					enabled = true,
-					required_width = 110, -- min width of window required to show this column
-				},
-				symlink_target = {
-					enabled = false,
-				},
+			modified = {
+				symbol = "[+]",
+				highlight = "NeoTreeModified",
 			},
-			-- A list of functions, each representing a global custom command
-			-- that will be available in all sources (if not overridden in `opts[source_name].commands`)
-			-- see `:h neo-tree-custom-commands-global`
-			commands = {},
+			name = {
+				trailing_slash = true,
+				use_git_status_colors = true,
+				highlight = "NeoTreeFileName",
+			},
 			window = {
 				position = "float",
 				width = 35,
@@ -212,7 +111,6 @@ return {
 					["i"] = "show_file_details",
 				},
 			},
-			nesting_rules = {},
 			filesystem = {
 				filtered_items = {
 					visible = false, -- when true, they will just be displayed differently than normal items
@@ -289,8 +187,6 @@ return {
 						-- ['<key>'] = function(state, scroll_padding) ... end,
 					},
 				},
-
-				commands = {}, -- Add a custom command or override a global one using the same function name
 			},
 			buffers = {
 				follow_current_file = {
