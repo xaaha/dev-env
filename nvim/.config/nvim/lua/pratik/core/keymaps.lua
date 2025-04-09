7a83b90fd6939a67ce89986e43c3d7ee9d2bf281
xaaha <pratik.philos@yahoo.com>
2025-04-09 10:55:37 -0400

    keymap

--- nvim/.config/nvim/lua/pratik/core/keymaps.lua
+++ nvim/.config/nvim/lua/pratik/core/keymaps.lua
@@ -12,6 +12,7 @@ keymap.set("n", "<leader>ss", "<cmd>wa<CR>", { desc = "Save all" })
 keymap.set("n", "<leader> ", "<cmd>wa<CR>", { desc = "Save current file" })
 keymap.set("n", "<leader>qq", "<cmd>wqa<CR>", { desc = "Write quit all" })
 keymap.set("v", "p", '"_dP', { desc = "Don't override yank on visual mode" })
+keymap.set("n", "dd", '"_dd', { desc = "Delete a line without overriding the yank register" })
 
 -- use jk & kj  to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
