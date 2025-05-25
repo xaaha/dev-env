d600be1c985a6c9daddb2fc7b924cedd63743fec
Pratik Thapa <pratik.philos@live.com>
2025-05-25 15:59:14 -0400

    updates and removing nvim-cmp

--- nvim/.config/nvim/lua/xaaha/plugins/lsp/lspconfig.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/lsp/lspconfig.lua
@@ -2,8 +2,8 @@ return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
 	dependencies = {
-		"hrsh7th/cmp-nvim-lsp",
 		{ "antosha417/nvim-lsp-file-operations", config = true },
+		"saghen/blink.cmp",
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
