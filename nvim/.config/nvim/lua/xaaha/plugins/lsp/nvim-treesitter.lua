ed653bde6aa07758e9e946827ca8f717d0974c53
Pratik Thapa <pratik.philos@live.com>
2025-05-26 15:10:06 -0400

    removes comment.nvim as nvim has native support for it

--- nvim/.config/nvim/lua/xaaha/plugins/lsp/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/lsp/nvim-treesitter.lua
@@ -51,6 +51,22 @@ return {
 			if #parsers_to_install > 0 then
 				ts.install(parsers_to_install)
 			end
+
+			-- copied this piece of code from reddit:
+			-- https://www.reddit.com/r/neovim/comments/1kuj9xm/has_anyone_successfully_switched_to_the_new/
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function(details)
+					local bufnr = details.buf
+					if not pcall(vim.treesitter.start, bufnr) then -- try to start treesitter which enables syntax highlighting
+						return -- Exit if treesitter was unable to start
+					end
+					vim.bo[bufnr].syntax = "on" -- Use regex based syntax-highlighting as fallback as some plugins might need it
+					vim.wo.foldlevel = 99
+					vim.wo.foldmethod = "expr"
+					vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()" -- Use treesitter for folds
+					vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" -- Use treesitter for indentation
+				end,
+			})
 		end,
 	},
 	{
