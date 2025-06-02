c44e3177c65eae9a14b8988ed63ef4993355bcb2
xaaha <pratik.philos@yahoo.com>
2025-06-02 16:38:44 -0400

    use prettier for astro files

--- nvim/.config/nvim/lua/xaaha/plugins/lsp/formatting.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/lsp/formatting.lua
@@ -7,6 +7,7 @@ return {
 
 		conform.setup({
 			formatters_by_ft = {
+				astro = { "prettier" },
 				javascript = { "biome", "prettier" },
 				typescript = { "biome", "prettier" },
 				javascriptreact = { "biome", "prettier" },
