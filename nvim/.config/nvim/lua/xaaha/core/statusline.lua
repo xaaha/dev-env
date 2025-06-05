24619c4fedd290c887bfe4b2e05d9db974a0fdc4
xaaha <pratik.philos@yahoo.com>
2025-06-05 10:36:36 -0400

    add reddit link

--- nvim/.config/nvim/lua/xaaha/core/statusline.lua
+++ nvim/.config/nvim/lua/xaaha/core/statusline.lua
@@ -1,3 +1,5 @@
+-- Copied and Modified from this reddit post
+-- https://www.reddit.com/r/neovim/comments/17hbep3/anyone_built_a_statusline_with_no_plugins/
 local statusline = {
 	" %t", -- File name
 	" %r", -- Readonly flag
