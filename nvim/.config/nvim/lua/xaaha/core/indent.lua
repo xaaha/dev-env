95b4bc7b1eac409e33897eba5f86d2f11630605c
Pratik Thapa <pratik.philos@live.com>
2025-07-17 19:39:04 -0400

    minimal indnet. That's always on

--- nvim/.config/nvim/lua/xaaha/core/indent.lua
+++ nvim/.config/nvim/lua/xaaha/core/indent.lua
@@ -1,11 +1,11 @@
 local ns = vim.api.nvim_create_namespace("indent_guides")
 local indent_char = "│"
 local indent_hl = "TroubleIndent"
-local indent_priority = 10 -- just in case another setup tries to override
+local indent_priority = 10
 
 vim.api.nvim_set_hl(0, indent_hl, { link = "NonText", default = true })
 
--- Get indent level in spaces
+-- Get effective indent for a line
 local function get_indent(lnum)
 	local indent = vim.fn.indent(lnum)
 	if indent > 0 or vim.fn.getline(lnum):match("%S") then
@@ -18,48 +18,45 @@ local function get_indent(lnum)
 		return 0
 	end
 
-	local indent_prev = prev > 0 and vim.fn.indent(prev) or 0
-	local indent_next = next > 0 and vim.fn.indent(next) or 0
-
-	if indent_prev ~= indent_next and indent_prev > 0 and indent_next > 0 then
-		return math.min(indent_prev, indent_next) + vim.bo.shiftwidth
+	local ip, in_ = vim.fn.indent(prev), vim.fn.indent(next)
+	if ip ~= in_ and ip > 0 and in_ > 0 then
+		return math.min(ip, in_) + vim.bo.shiftwidth
 	end
-
-	return math.min(indent_prev, indent_next)
+	return math.min(ip, in_)
 end
 
--- Generate extmarks for a given line's indent level
-local function get_extmarks(indent, shiftwidth, leftcol)
-	local extmarks = {}
-	local levels = math.floor(indent / shiftwidth)
-	for i = 1, levels do
-		local col = (i - 1) * shiftwidth - leftcol
+-- Generate extmarks based on indent
+local function get_extmarks(indent, sw, leftcol)
+	local marks = {}
+	for i = 0, math.floor(indent / sw) - 1 do
+		local col = i * sw - leftcol
 		if col >= 0 then
-			table.insert(extmarks, {
+			marks[#marks + 1] = {
 				virt_text = { { indent_char, indent_hl } },
 				virt_text_win_col = col,
 				hl_mode = "combine",
-				priority = indent_priority, -- so that other plugin won't be able to overwrite this.
+				priority = indent_priority,
 				ephemeral = true,
-			})
+			}
 		end
 	end
-	return extmarks
+	return marks
 end
 
--- Decoration callback: runs during redraws
+-- Register decoration provider
 vim.api.nvim_set_decoration_provider(ns, {
 	on_win = function(_, _, buf, top, bot)
 		if vim.bo[buf].buftype ~= "" then
 			return
 		end
-		local shiftwidth = vim.bo[buf].shiftwidth > 0 and vim.bo[buf].shiftwidth or vim.bo[buf].tabstop
+
+		local sw = vim.bo[buf].shiftwidth > 0 and vim.bo[buf].shiftwidth or vim.bo[buf].tabstop
 		local leftcol = vim.fn.winsaveview().leftcol
+
 		for lnum = top + 1, bot do
 			local indent = get_indent(lnum)
 			if indent > 0 then
-				local extmarks = get_extmarks(indent, shiftwidth, leftcol)
-				for _, mark in ipairs(extmarks) do
+				for _, mark in ipairs(get_extmarks(indent, sw, leftcol)) do
 					vim.api.nvim_buf_set_extmark(buf, ns, lnum - 1, 0, mark)
 				end
 			end
