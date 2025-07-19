fe985ed715028b4c4d51ecb44bc0dc651e7d8c85
Pratik Thapa <pratik.philos@live.com>
2025-07-19 16:34:10 -0400

    rm markdown preview
    never used it
    and updates

--- nvim/.config/nvim/lua/xaaha/plugins/markdown.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/markdown.lua
@@ -1,26 +1,5 @@
 return {
-	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		opts = {},
-		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	},
-	{
-		--Run :Lazy build markdown-preview.nvim
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function(plugin)
-			if vim.fn.executable("npx") then
-				vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
-			else
-				vim.cmd([[Lazy load markdown-preview.nvim]])
-				vim.fn["mkdp#util#install"]()
-			end
-		end,
-		init = function()
-			if vim.fn.executable("npx") then
-				vim.g.mkdp_filetypes = { "markdown" }
-			end
-		end,
-	},
+	"MeanderingProgrammer/render-markdown.nvim",
+	opts = {},
+	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
 }
