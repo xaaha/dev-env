3fa372a6793a1a3eb6fd498410d1e61506ecdf3f
Pratik Thapa <pratik.philos@live.com>
2025-08-07 13:45:49 -0400

    use nvim

--- nvim/.config/nvim/lua/xaaha/plugins/misc.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/misc.lua
@@ -1,6 +1,5 @@
 return {
 	-- "RRethy/vim-illuminate", -- highlight other uses of the word under the cursor
 	"b0o/schemastore.nvim",
-	"preservim/vimux", -- vim test requires this
 	"nvim-tree/nvim-web-devicons",
 }
--- nvim/.config/nvim/lua/xaaha/plugins/vim-test.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/vim-test.lua
@@ -1,12 +1,9 @@
 return {
 	"vim-test/vim-test",
-	dependencies = {
-		"preservim/vimux",
-	},
 	-- vim.keymap.set("n", "<leader>T", ":TestNearest<CR>", { desc = "Test nearest test" }),
 	vim.keymap.set("n", "<leader>tt", ":TestFile<CR>", { desc = "Test this file" }),
 	vim.keymap.set("n", "<leader>ts", ":TestSuite<CR>", { desc = "Test entire suite" }),
 	-- vim.keymap.set("n", "<leader>l", ":TestLast<CR>"),
 	-- vim.keymap.set("n", "<leader>g", ":TestVisit<CR>"),
-	vim.cmd("let test#strategy = 'vimux'"),
+	vim.cmd("let test#strategy = 'neovim_sticky'"),
 }
