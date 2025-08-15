7d8c560079b7a7824383a50c66dc3f92ca2a80cc
Pratik Thapa <pratik.philos@live.com>
2025-08-14 21:17:26 -0400

    use lazy for markdown

--- nvim/.config/nvim/lua/xaaha/plugins/markdown.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/markdown.lua
@@ -2,4 +2,6 @@ return {
 	"MeanderingProgrammer/render-markdown.nvim",
 	opts = {},
 	dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
 }
