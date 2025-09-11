10062e4cab461f6420c6cd4ddca75b9dbab6ce04
xaaha <pratik.philos@gmail.com>
2025-09-11 15:47:39 -0400

    add resume keybining

--- nvim/.config/nvim/lua/xaaha/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/fzf-lua.lua
@@ -64,6 +64,7 @@ return {
 		customKeymap("n", "<leader>ft", "<cmd>FzfLua git_status<CR>", { desc = "Find git_status files" })
 		customKeymap("n", "<leader>fs", "<cmd>FzfLua lsp_document_symbols<CR>", { desc = "Find lsp_document_symbols" })
 		customKeymap("n", "<leader>fk", "<cmd>FzfLua keymaps<CR>", { desc = "Find Keymaps" })
+		customKeymap("n", "<c-m>", "<cmd>FzfLua resume<CR>", { desc = "Resume Last Fzf Search" })
 		customKeymap(
 			"n",
 			"<leader>bd",
