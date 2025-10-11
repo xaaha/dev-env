388bad23f31265e72c3dedb7c79ca0c02bdab47b
xaaha <pratik.philos@gmail.com>
2025-10-11 11:45:38 -0400

    update winbar

--- nvim/.config/nvim/lua/xaaha/core/winbar.lua
+++ nvim/.config/nvim/lua/xaaha/core/winbar.lua
@@ -1,17 +1,4 @@
-local function isempty(s)
-	return s == nil or s == ""
-end
-
-local function get_buf_option(opt)
-	local status_ok, buf_option = pcall(vim.api.nvim_buf_get_option, 0, opt)
-	if not status_ok then
-		return nil
-	else
-		return buf_option
-	end
-end
-
-local winbar_filetype_exclude = {
+local excluded = {
 	"help",
 	"startify",
 	"dashboard",
@@ -26,86 +13,55 @@ local winbar_filetype_exclude = {
 	"toggleterm",
 }
 
-local get_filename = function()
+local function isempty(s)
+	return s == nil or s == ""
+end
+
+local function get_filename()
 	local filename = vim.fn.expand("%:t")
-	local extension = vim.fn.expand("%:e")
-
-	if not isempty(filename) then
-		local file_icon, file_icon_color =
-			require("nvim-web-devicons").get_icon_color(filename, extension, { default = true })
-
-		local hl_group = "FileIconColor" .. extension
-
-		vim.api.nvim_set_hl(0, hl_group, { fg = file_icon_color })
-		if isempty(file_icon) then
-			file_icon = ""
-			file_icon_color = ""
-		end
-
-		return " " .. "%#" .. hl_group .. "#" .. file_icon .. "%*" .. " " .. "%#LineNr# " .. filename .. " %*"
-	end
-end
-
-local get_gps = function()
-	local status_gps_ok, gps = pcall(require, "nvim-gps")
-	if not status_gps_ok then
+	if isempty(filename) then
 		return ""
 	end
 
-	local status_ok, gps_location = pcall(gps.get_location, {})
-	if not status_ok then
+	local ext = vim.fn.expand("%:e")
+	local icon, color = require("nvim-web-devicons").get_icon_color(filename, ext, { default = true })
+	local hl_group = "WinbarFileIcon_" .. ext
+
+	vim.api.nvim_set_hl(0, hl_group, { fg = color, bg = "NONE" })
+
+	return string.format(" %%#%s#%s %%* %%#StatusLine#%s%%*", hl_group, icon or "", filename)
+end
+
+local function get_winbar()
+	if vim.tbl_contains(excluded, vim.bo.filetype) then
 		return ""
 	end
 
-	if not gps.is_available() or gps_location == "error" then
+	local name = get_filename()
+	if isempty(name) then
 		return ""
 	end
 
-	if not isempty(gps_location) then
-		return "" .. " " .. gps_location
-	else
-		return ""
-	end
+	return "%#StatusLine#" .. name .. "%="
 end
 
-local excludes = function()
-	if vim.tbl_contains(winbar_filetype_exclude, vim.bo.filetype) then
-		vim.opt_local.winbar = nil
-		return true
-	end
-	return false
+local function setup()
+	vim.api.nvim_set_hl(0, "WinBar", { link = "StatusLine" })
+	vim.api.nvim_set_hl(0, "WinBarNC", { link = "StatusLineNC" })
+	vim.api.nvim_set_hl(0, "WinBarFile", { link = "Normal" })
+	vim.api.nvim_set_hl(0, "WinBarFill", { link = "Normal" })
+
+	-- Update on buffer change
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+		callback = function()
+			local winbar_str = get_winbar()
+			if isempty(winbar_str) then
+				vim.opt_local.winbar = nil
+			else
+				vim.opt_local.winbar = winbar_str
+			end
+		end,
+	})
 end
 
-local get_winbar = function()
-	if excludes() then
-		return
-	end
-	local value = get_filename()
-
-	local gps_added = false
-	if not isempty(value) then
-		local gps_value = get_gps()
-		value = value .. " " .. gps_value
-		if not isempty(gps_value) then
-			gps_added = true
-		end
-	end
-
-	if not isempty(value) and get_buf_option("mod") then
-		local mod = "%#LineNr#" .. "%*"
-		if gps_added then
-			value = value .. " " .. mod
-		else
-			value = value .. mod
-		end
-	end
-
-	local status_ok, _ = pcall(vim.api.nvim_set_option_value, "winbar", value, { scope = "local" })
-	if not status_ok then
-		return
-	end
-end
-
-vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-	callback = get_winbar,
-})
+setup()
