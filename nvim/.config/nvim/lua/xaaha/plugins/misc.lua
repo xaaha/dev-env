5d2516f961a7e5d8fd9f7dceb4a7f7052cbd6146
xaaha <pratik.philos@gmail.com>
2025-10-12 22:32:51 -0400

    move lspconfi to misc

--- nvim/.config/nvim/lua/xaaha/plugins/misc.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/misc.lua
@@ -1,4 +1,8 @@
 return {
-	"b0o/schemastore.nvim",
-	"nvim-tree/nvim-web-devicons",
+  "b0o/schemastore.nvim",
+  "nvim-tree/nvim-web-devicons",
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+  }
 }
