46be19a97370bb9d2e3e5a0885b864bc5a14a8df
xaaha <pratik.philos@gmail.com>
2025-10-12 22:33:06 -0400

    rm markdown
    too heavy

--- nvim/.config/nvim/lua/xaaha/plugins/markdown.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"OXY2DEV/markview.nvim",
-	opts = {},
-	-- dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
-	lazy = true,
-	event = { "BufReadPre", "BufNewFile" },
-}
