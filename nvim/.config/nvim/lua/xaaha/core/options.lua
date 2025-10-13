c794544b3e301c63625ea0205888b7f0340c2f32
xaaha <pratik.philos@gmail.com>
2025-10-13 17:39:31 -0400

    updates and wrap words in md

--- nvim/.config/nvim/lua/xaaha/core/options.lua
+++ nvim/.config/nvim/lua/xaaha/core/options.lua
@@ -2,12 +2,12 @@ local opt = vim.opt -- for conciseness
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2    -- 2 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
@@ -15,7 +15,7 @@ opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -24,7 +24,7 @@ opt.cursorline = true -- highlight the current cursor line
 -- need iTerm2 or any true Color Terminal
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -47,14 +47,24 @@ opt.scrolloff = 8 -- Minimum number of line to keep at the top or bottom
 -- 999 to start scroll when the cursor is at the middle position
 
 -- editing modes
-opt.virtualedit = "block" -- lets you move to the end of line in Visual block mode even after end character. :h virtualedit
+opt.virtualedit =
+"block"                  -- lets you move to the end of line in Visual block mode even after end character. :h virtualedit
 opt.inccommand = "split" -- groups the global search and replace to a window below with a line number
 
 -- highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "highlight on Yank",
-	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "highlight on Yank",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
+-- wrap lines and words on .txt and .md files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "text" },
+  callback = function()
+    vim.opt_local.wrap = true
+    vim.opt_local.linebreak = true
+  end,
 })
