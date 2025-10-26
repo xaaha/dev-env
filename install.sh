8c5e2ad07cd442f9c2c4d88a548034913dc43846
xaaha <pratik.philos@gmail.com>
2025-10-26 12:36:17 -0400

    use brewfile

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Check if Xcode command line tools are installed
-if ! xcode-select -p &> /dev/null
-then
+if ! xcode-select -p &> /dev/null; then
     echo "Xcode Command Line Tools not found, installing..."
     xcode-select --install
 
@@ -18,8 +17,7 @@ else
 fi
 
 # Check if Homebrew is installed, install if not
-if ! command -v brew &> /dev/null
-then
+if ! command -v brew &> /dev/null; then
     echo "Homebrew not found, installing..."
 
     # Request sudo upfront for Homebrew installation
@@ -34,7 +32,6 @@ then
         eval "$(/opt/homebrew/bin/brew shellenv)"
         echo "Homebrew path added to ~/.bash_profile"
     elif [[ "$SHELL" == "/bin/zsh" ]]; then
-        echo >> ~/.zprofile
         echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
         eval "$(/opt/homebrew/bin/brew shellenv)"
         echo "Homebrew path added to ~/.zprofile"
@@ -43,38 +40,13 @@ else
     echo "Homebrew is already installed."
 fi
 
-# Function to process the install.txt content
-process_items() {
-    while IFS= read -r item
-    do
-        # Skip empty lines or lines starting with a comment (#)
-        if [[ -z "$item" || "$item" == \#* ]]; then
-            continue
-        fi
-
-        # Check if the item is already installed
-        if brew list --formula | grep -q "^${item}$" || brew list --cask | grep -q "^${item}$"; then
-            echo "$item is already installed, skipping..."
-            continue
-        fi
-
-        # If the line contains --cask, install it as a cask
-        if [[ "$item" == *"--cask"* ]]; then
-            echo "Installing $item as a cask..."
-            brew install --cask "${item/--cask /}"
-        else
-            echo "Installing $item..."
-            brew install "$item"
-        fi
-    done
-}
-
-# Check if install.txt is available locally
-if [ -f install.txt ]; then
-    process_items < install.txt
+if [ -f Brewfile ]; then
+    echo "Brewfile not found locally. Fetching from GitHub..."
+    curl -s -O https://raw.githubusercontent.com/xaaha/dev-env/refs/heads/main/BrewFile 
+    brew bundle --file=Brewfile
 else
-    echo "install.txt not found locally. Fetching from GitHub..."
-    curl -s https://raw.githubusercontent.com/xaaha/dev-env/main/install.txt | process_items
+    echo "Could not fetch githubusercontent, try providing local brew file \n"
+    exit 1
 fi
 
 echo "Installation complete."
