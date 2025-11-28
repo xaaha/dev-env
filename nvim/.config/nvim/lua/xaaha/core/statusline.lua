0b6404f76f20d292b47419860dc2986e2617791b
Pratik Thapa <pratikthapa@Pratiks-MacBook-Pro.local>
2025-11-27 23:00:20 -0500

    move branch on the right

--- nvim/.config/nvim/lua/xaaha/core/statusline.lua
+++ nvim/.config/nvim/lua/xaaha/core/statusline.lua
@@ -1,27 +1,26 @@
 local sep_right = "" -- 
--- local chevron_left = "\u{eab5}" -- 
+local chevron_left = "\u{eab5}" -- 
 
 local function git_branch()
   local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
   if not branch or branch == "" or branch == "HEAD" then
     return ""
   end
-  branch = #branch > 20 and branch:sub(-20) or branch
-  return " " .. branch .. " " .. sep_right .. " "
+  branch = #branch > 30 and branch:sub(-30) or branch
+  return "  " .. branch .. " " --.. sep_right .. " "
 end
 
 local statusline = table.concat({
   " %f " .. sep_right, -- File name/path
   " %r",               -- Readonly flag
   "%#GitBranch#",      -- Git highlight start
-  "%{v:lua.git_branch()}",
   "%*",                -- Reset highlight
   "%#GitChanges#",
   "%{get(b:,'gitsigns_status','')}",
   "%*",
   " %m ", -- Modified flag
-  -- "%=",                              -- divider
-  --  .. "%{get(b:, 'gitsigns_blame_line', '')} ",
+  "%=",   -- divider
+  chevron_left .. "%{v:lua.git_branch()}",
 })
 
 _G.git_branch = git_branch
