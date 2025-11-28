a3e5857bc3767e36fdc3826068019afbfb53ca82
Pratik Thapa <pratikthapa@Pratiks-MacBook-Pro.local>
2025-11-27 22:49:28 -0500

    minimal statusline

--- nvim/.config/nvim/lua/xaaha/core/statusline.lua
+++ nvim/.config/nvim/lua/xaaha/core/statusline.lua
@@ -1,44 +1,29 @@
-local chevron_right = "\u{eab6}" -- 
-local chevron_left = "\u{eab5}"  -- 
+local sep_right = "" -- 
+-- local chevron_left = "\u{eab5}" -- 
 
---- Gets the current Git branch name
----@return string
-local function get_git_branch()
-  local handle = io.popen("git rev-parse --abbrev-ref HEAD 2>/dev/null")
-  if handle then
-    local branch = handle:read("*a") -- Read the output of the command
-    handle:close()
-    branch = branch:gsub("%s+", "")  -- Remove any trailing whitespace
-    return branch
+local function git_branch()
+  local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
+  if not branch or branch == "" or branch == "HEAD" then
+    return ""
   end
-  return "" -- Return an empty string if the command fails
+  branch = #branch > 20 and branch:sub(-20) or branch
+  return " " .. branch .. " " .. sep_right .. " "
 end
 
---- Git component for the statusline
----@return string
-local function git_component()
-  local branch = get_git_branch()
-  if branch == "" then
-    return "" -- No branch detected
-  end
-
-  -- Trim the branch name to the last 10 characters if it's too long
-  local trimmed_branch = #branch > 20 and branch:sub(-20) or branch
-  return string.format(" %s " .. chevron_right .. " ", trimmed_branch)
-end
-
-local statusline = {
-  " %f " .. chevron_right, -- File path. If you need to trim the path use, "[%<%.30f]"
-  " %r",                   -- Readonly flag
-  "%#GitBranch#",          -- Start git component highlight
-  git_component(),
-  "%*",                    -- Reset to default highlight
+local statusline = table.concat({
+  " %f " .. sep_right, -- File name/path
+  " %r",               -- Readonly flag
+  "%#GitBranch#",      -- Git highlight start
+  "%{v:lua.git_branch()}",
+  "%*",                -- Reset highlight
   "%#GitChanges#",
   "%{get(b:,'gitsigns_status','')}",
-  "%*",   -- Reset to default highlight
+  "%*",
   " %m ", -- Modified flag
-  "%=",   -- divider
-  chevron_left .. " Line: %l Column: %c ",
-}
+  -- "%=",                              -- divider
+  --  .. "%{get(b:, 'gitsigns_blame_line', '')} ",
+})
 
-vim.o.statusline = table.concat(statusline, "")
+_G.git_branch = git_branch
+
+vim.o.statusline = statusline
