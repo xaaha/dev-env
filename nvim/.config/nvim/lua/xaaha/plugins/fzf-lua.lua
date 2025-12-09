b6553db477a05f4a792b1efa86be998e3f7c391b
xaaha <pratik.philos@gmail.com>
2025-12-09 12:24:59 -0500

    updates

--- nvim/.config/nvim/lua/xaaha/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/xaaha/plugins/fzf-lua.lua
@@ -58,7 +58,7 @@ return {
     local customKeymap = vim.api.nvim_set_keymap
     customKeymap(
       "n",
-      "<C-i>",
+      "<leader>rg",
       "<cmd>FzfLua live_grep<CR>",
       { noremap = true, silent = true, desc = "Fzf live grep native" }
     )
