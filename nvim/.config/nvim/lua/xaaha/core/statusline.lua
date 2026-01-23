c1b2e7c878b00d0808027c63b31549fba0bac04a
xaaha <pratik.philos@gmail.com>
2026-01-23 13:40:55 -0500

    ref sl

--- nvim/.config/nvim/lua/xaaha/core/statusline.lua
+++ nvim/.config/nvim/lua/xaaha/core/statusline.lua
@@ -1,26 +1,26 @@
-local sep_right = "" -- 
-local chevron_left = "\u{eab5}" -- 
+local chevron_left = ""
+local chevron_right = ""
 
 local function git_branch()
   local branch = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD 2>/dev/null")[1]
   if vim.v.shell_error ~= 0 or not branch or branch == "" then
-    return ""
+    return "Not a Git Repo "
   end
   branch = #branch > 30 and branch:sub(-30) or branch
   return "  " .. branch .. " "
 end
 
 local statusline = table.concat({
-  " %f " .. sep_right, -- File name/path
-  " %r",               -- Readonly flag
-  "%#GitBranch#",      -- Git highlight start
-  "%*",                -- Reset highlight
+  " %f " .. chevron_left, -- File name/path
+  " %r",                  -- Readonly flag
+  "%#GitBranch#",         -- Git highlight start
+  "%*",                   -- Reset highlight
   "%#GitChanges#",
   "%{get(b:,'gitsigns_status','')}",
   "%*",
   " %m ", -- Modified flag
   "%=",   -- divider
-  chevron_left .. "%{v:lua.git_branch()}",
+  chevron_right .. " %{v:lua.git_branch()}",
 })
 
 _G.git_branch = git_branch
