b9c94b78c6dfd53be98b91f070d8c09175d92236
Pratik Thapa <pratikthapa@Pratiks-MacBook-Pro.local>
2026-01-24 22:55:41 -0500

    nrodic and github_dark

---
+++ nvim/.config/nvim/colors/github_dark.lua
@@ -0,0 +1,231 @@
+local M = {}
+
+-- Official github_dark palette: https://github.com/projekt0n/github-nvim-theme
+-- Primitives from: lua/github-theme/palette/primitives/dark.lua
+M.colors = {
+  -- Scale: Gray (index 1-10, light to dark)
+  -- #f0f6fc, #c9d1d9, #b1bac4, #8b949e, #6e7681, #484f58, #30363d, #21262d, #161b22, #0d1117
+
+  -- Backgrounds
+  black = "#0d1117",        -- canvas.default (gray[10])
+  darker_black = "#010409", -- scale.black
+  black2 = "#161b22",       -- canvas.overlay (gray[9])
+  one_bg = "#21262d",       -- canvas.subtle (gray[8])
+  one_bg2 = "#30363d",      -- border.default (gray[7])
+  one_bg3 = "#484f58",      -- border.muted (gray[6])
+
+  -- Greys
+  grey = "#6e7681",       -- gray[5] - neutral.muted
+  grey_fg = "#8b949e",    -- gray[4] - fg.muted / comment
+  grey_fg2 = "#8b949e",   -- gray[4]
+  light_grey = "#b1bac4", -- gray[3]
+
+  -- Foregrounds
+  white = "#c9d1d9",        -- fg.default (gray[2])
+  darker_white = "#e6edf3", -- fg.default alternate
+  bright_white = "#f0f6fc", -- gray[1] - brightest
+
+  -- Blues (scale: #cae8ff, #a5d6ff, #79c0ff, #58a6ff, #388bfd, #1f6feb, #1158c7, #0d419d, #0c2d6b, #051d4d)
+  blue = "#58a6ff",       -- blue[4] - accent.fg
+  blue_bright = "#79c0ff", -- blue[3] - constant color
+  blue_light = "#a5d6ff",  -- blue[2] - string color
+  nord_blue = "#388bfd",   -- blue[5]
+  blue_dark = "#1f6feb",   -- blue[6]
+
+  -- Greens (scale: #aff5b4, #7ee787, #56d364, #3fb950, #2ea043, #238636, #196c2e, #0f5323, #033a16, #04260f)
+  green = "#3fb950",        -- green[4] - success.fg
+  vibrant_green = "#56d364", -- green[3]
+  green_bright = "#7ee787", -- green[2]
+
+  -- Reds (scale: #ffdcd7, #ffc1ba, #ffa198, #ff7b72, #f85149, #da3633, #b62324, #8e1519, #67060c, #490202)
+  red = "#ff7b72",       -- red[4] - keyword color
+  red_bright = "#ffa198", -- red[3]
+  dark_red = "#f85149",  -- red[5] - danger.fg
+
+  -- Oranges (scale: #ffdfb6, #ffc680, #ffa657, #f0883e, #db6d28, #bd561d, #9b4215, #762d0a, #5a1e02, #3d1300)
+  orange = "#ffa657",      -- orange[3] - variable/type color
+  orange_bright = "#ffc680", -- orange[2]
+  orange_dark = "#f0883e", -- orange[4]
+
+  -- Yellows (scale: #f8e3a1, #f2cc60, #e3b341, #d29922, #bb8009, #9e6a03, #845306, #693e00, #4b2900, #341a00)
+  yellow = "#d29922",       -- yellow[4] - attention.fg
+  yellow_bright = "#e3b341", -- yellow[3]
+  sun = "#f2cc60",          -- yellow[2]
+
+  -- Purples (scale: #eddeff, #e2c5ff, #d2a8ff, #bc8cff, #a371f7, #8957e5, #6e40c9, #553098, #3c1e70, #271052)
+  purple = "#d2a8ff",      -- purple[3] - entity/function color
+  purple_bright = "#e2c5ff", -- purple[2]
+  dark_purple = "#a371f7", -- purple[5] - done.fg
+
+  -- Pinks (scale: #ffdaec, #ffbedd, #ff9bce, #f778ba, #db61a2, #bf4b8a, #9e3670, #7d2457, #5e103e, #42062a)
+  pink = "#f778ba",      -- pink[4]
+  baby_pink = "#ff9bce", -- pink[3]
+
+  -- Cyans
+  cyan = "#76e3ea",       -- cyan.fg
+  cyan_bright = "#b3f0ff", -- cyan.bright
+  teal = "#3fb950",       -- using green for teal
+
+  -- UI colors
+  line = "#21262d",          -- border.muted
+  statusline_bg = "#161b22", -- canvas.overlay
+  lightbg = "#21262d",       -- canvas.subtle
+  pmenu_bg = "#58a6ff",      -- accent.fg
+  folder_bg = "#58a6ff",     -- accent.fg
+
+  -- Git colors
+  git_add = "#3fb950",    -- success.fg
+  git_change = "#d29922", -- attention.fg
+  git_delete = "#f85149", -- danger.fg
+
+  -- Base16 mapping for github_dark
+  base00 = "#0d1117", -- bg
+  base01 = "#161b22", -- lighter bg
+  base02 = "#21262d", -- selection
+  base03 = "#30363d", -- comments base
+  base04 = "#484f58", -- dark fg
+  base05 = "#c9d1d9", -- fg
+  base06 = "#e6edf3", -- light fg
+  base07 = "#f0f6fc", -- bright fg
+  base08 = "#ff7b72", -- red - variables/keywords
+  base09 = "#ffa657", -- orange - types/variables
+  base0A = "#d29922", -- yellow - classes
+  base0B = "#a5d6ff", -- light blue - strings
+  base0C = "#79c0ff", -- blue - constants/support
+  base0D = "#d2a8ff", -- purple - functions
+  base0E = "#ff7b72", -- red - keywords
+  base0F = "#f85149", -- dark red - special
+}
+
+-- Override highlights to match original github-nvim-theme behavior
+M.highlights = {
+  -- Functions use purple (entity color)
+  Function = { fg = M.colors.purple },
+  ["@function"] = { fg = M.colors.purple },
+  ["@function.builtin"] = { fg = M.colors.purple },
+  ["@function.call"] = { fg = M.colors.purple },
+  ["@method"] = { fg = M.colors.purple },
+  ["@method.call"] = { fg = M.colors.purple },
+  ["@function.method.call"] = { fg = M.colors.purple },
+
+  -- Keywords use red
+  Keyword = { fg = M.colors.red },
+  Statement = { fg = M.colors.red },
+  Conditional = { fg = M.colors.red },
+  Repeat = { fg = M.colors.red },
+  ["@keyword"] = { fg = M.colors.red },
+  ["@keyword.function"] = { fg = M.colors.red },
+  ["@keyword.return"] = { fg = M.colors.red },
+  ["@keyword.operator"] = { fg = M.colors.red },
+  ["@conditional"] = { fg = M.colors.red },
+  ["@repeat"] = { fg = M.colors.red },
+
+  -- Types use orange (variable color in github)
+  Type = { fg = M.colors.orange },
+  ["@type"] = { fg = M.colors.orange },
+  ["@type.builtin"] = { fg = M.colors.orange },
+
+  -- Variables use fg
+  ["@variable"] = { fg = M.colors.white },
+  ["@variable.builtin"] = { fg = M.colors.blue_bright },
+
+  -- Parameters
+  ["@parameter"] = { fg = M.colors.white },
+  ["@variable.parameter"] = { fg = M.colors.white },
+
+  -- Properties/fields
+  ["@property"] = { fg = M.colors.blue_bright },
+  ["@field"] = { fg = M.colors.blue_bright },
+  ["@variable.member"] = { fg = M.colors.blue_bright },
+
+  -- Constants use blue (constant color)
+  Constant = { fg = M.colors.blue_bright },
+  ["@constant"] = { fg = M.colors.blue_bright },
+  ["@constant.builtin"] = { fg = M.colors.blue_bright },
+
+  -- Numbers use blue (constant color)
+  Number = { fg = M.colors.blue_bright },
+  Float = { fg = M.colors.blue_bright },
+  Boolean = { fg = M.colors.blue_bright },
+  ["@number"] = { fg = M.colors.blue_bright },
+  ["@float"] = { fg = M.colors.blue_bright },
+  ["@boolean"] = { fg = M.colors.blue_bright },
+
+  -- Strings use light blue (string color)
+  String = { fg = M.colors.blue_light },
+  ["@string"] = { fg = M.colors.blue_light },
+  Character = { fg = M.colors.blue_light },
+  ["@character"] = { fg = M.colors.blue_light },
+
+  -- Operators use blue (constant color)
+  Operator = { fg = M.colors.blue_bright },
+  ["@operator"] = { fg = M.colors.blue_bright },
+
+  -- Punctuation
+  ["@punctuation.bracket"] = { fg = M.colors.white },
+  ["@punctuation.delimiter"] = { fg = M.colors.white },
+  ["@punctuation.special"] = { fg = M.colors.blue_bright },
+  Delimiter = { fg = M.colors.white },
+
+  -- Constructors use orange
+  ["@constructor"] = { fg = M.colors.orange },
+
+  -- Include/imports use red (keyword)
+  Include = { fg = M.colors.red },
+  ["@include"] = { fg = M.colors.red },
+
+  -- PreProc use red
+  PreProc = { fg = M.colors.red },
+  Define = { fg = M.colors.red },
+
+  -- Special use blue
+  Special = { fg = M.colors.blue_bright },
+  SpecialChar = { fg = M.colors.blue_bright },
+
+  -- Comments use muted gray
+  Comment = { fg = M.colors.grey_fg, italic = true },
+  ["@comment"] = { fg = M.colors.grey_fg, italic = true },
+
+  -- Tags (HTML/JSX) use green
+  Tag = { fg = M.colors.green },
+  ["@tag"] = { fg = M.colors.green },
+  ["@tag.attribute"] = { fg = M.colors.blue_bright },
+  ["@tag.delimiter"] = { fg = M.colors.white },
+
+  -- Namespace/module
+  ["@namespace"] = { fg = M.colors.orange },
+  ["@module"] = { fg = M.colors.orange },
+
+  -- Labels
+  Label = { fg = M.colors.blue_bright },
+  ["@label"] = { fg = M.colors.blue_bright },
+
+  -- Storage
+  StorageClass = { fg = M.colors.red },
+  ["@storageclass"] = { fg = M.colors.red },
+
+  -- Structure
+  Structure = { fg = M.colors.red },
+
+  -- Typedef
+  Typedef = { fg = M.colors.orange },
+
+  -- Attribute
+  ["@attribute"] = { fg = M.colors.purple },
+
+  -- Escape sequences
+  ["@string.escape"] = { fg = M.colors.blue_bright },
+  ["@string.special"] = { fg = M.colors.blue_bright },
+
+  -- Regex
+  ["@string.regex"] = { fg = M.colors.blue_bright },
+
+  -- Builtin types
+  ["@type.qualifier"] = { fg = M.colors.red },
+
+  -- Exception
+  Exception = { fg = M.colors.red },
+  ["@exception"] = { fg = M.colors.red },
+}
+
+require("xaaha.core.colorscheme").setup_dark(M.colors, M.highlights)
---
+++ nvim/.config/nvim/colors/nordic.lua
@@ -0,0 +1,283 @@
+local M = {}
+
+-- Official nordic.nvim palette: https://github.com/AlexvZyl/nordic.nvim
+-- From: lua/nordic/colors/nordic.lua
+M.colors = {
+  -- Blacks (darker than nord)
+  black = "#191D24",        -- black0 - darkest
+  darker_black = "#1E222A", -- black1
+  black2 = "#222630",       -- black2
+
+  -- Grays
+  gray0 = "#242933",
+  gray1 = "#2E3440", -- nord bg equivalent
+  gray2 = "#3B4252",
+  gray3 = "#434C5E",
+  gray4 = "#4C566A",
+  gray5 = "#60728A", -- comment color
+
+  -- Backgrounds (using nordic's darker scheme)
+  one_bg = "#242933",  -- gray0
+  one_bg2 = "#2E3440", -- gray1
+  one_bg3 = "#3B4252", -- gray2
+
+  -- Greys for UI
+  grey = "#3B4252",       -- gray2
+  grey_fg = "#60728A",    -- gray5 - comments
+  grey_fg2 = "#4C566A",   -- gray4
+  light_grey = "#60728A", -- gray5
+
+  -- Whites
+  white = "#BBC3D4",        -- white0_normal - main fg
+  white0 = "#BBC3D4",       -- white0_normal
+  white0_rb = "#C0C8D8",    -- white0_reduce_blue
+  darker_white = "#D8DEE9", -- white1
+  white1 = "#D8DEE9",
+  white2 = "#E5E9F0",
+  bright_white = "#ECEFF4", -- white3
+  white3 = "#ECEFF4",
+
+  -- Blues
+  blue = "#81A1C1",      -- blue1 - main blue
+  blue0 = "#5E81AC",     -- blue0 - dark/builtin
+  blue1 = "#81A1C1",     -- blue1
+  blue2 = "#88C0D0",     -- blue2 - functions
+  nord_blue = "#81A1C1", -- blue1
+
+  -- Cyan (with variants)
+  cyan = "#8FBCBB",        -- cyan.base
+  cyan_bright = "#9FC6C5", -- cyan.bright
+  cyan_dim = "#80B3B2",    -- cyan.dim
+  teal = "#8FBCBB",        -- cyan.base
+
+  -- Red (with variants)
+  red = "#BF616A",        -- red.base
+  red_bright = "#C5727A", -- red.bright
+  red_dim = "#B74E58",    -- red.dim
+  dark_red = "#B74E58",   -- red.dim
+
+  -- Orange (with variants)
+  orange = "#D08770",        -- orange.base
+  orange_bright = "#D79784", -- orange.bright
+  orange_dim = "#CB775D",    -- orange.dim
+
+  -- Yellow (with variants)
+  yellow = "#EBCB8B",        -- yellow.base
+  yellow_bright = "#EFD49F", -- yellow.bright
+  yellow_dim = "#E7C173",    -- yellow.dim
+  sun = "#EFD49F",           -- yellow.bright
+
+  -- Green (with variants)
+  green = "#A3BE8C",        -- green.base
+  green_bright = "#B1C89D", -- green.bright
+  green_dim = "#97B67C",    -- green.dim
+  vibrant_green = "#B1C89D", -- green.bright
+
+  -- Magenta/Purple (with variants)
+  purple = "#B48EAD",        -- magenta.base
+  purple_bright = "#BE9DB8", -- magenta.bright
+  purple_dim = "#A97EA1",    -- magenta.dim
+  dark_purple = "#A97EA1",   -- magenta.dim
+
+  -- Pinks (using magenta variants)
+  pink = "#B48EAD",
+  baby_pink = "#BE9DB8",
+
+  -- UI colors
+  line = "#242933",          -- gray0
+  statusline_bg = "#1E222A", -- black1
+  lightbg = "#242933",       -- gray0
+  pmenu_bg = "#81A1C1",      -- blue1
+  folder_bg = "#81A1C1",     -- blue1
+
+  -- Diagnostic colors
+  error = "#BF616A",  -- red.base
+  warn = "#EBCB8B",   -- yellow.base
+  hint = "#B48EAD",   -- magenta.base
+  info = "#88C0D0",   -- blue2
+
+  -- Git colors
+  git_add = "#A3BE8C",    -- green.base
+  git_change = "#EBCB8B", -- yellow.base
+  git_delete = "#BF616A", -- red.base
+
+  -- Base16 mapping for nordic
+  base00 = "#191D24", -- bg (black0)
+  base01 = "#242933", -- lighter bg (gray0)
+  base02 = "#2E3440", -- selection (gray1)
+  base03 = "#3B4252", -- comments bg (gray2)
+  base04 = "#4C566A", -- dark fg (gray4)
+  base05 = "#BBC3D4", -- fg (white0)
+  base06 = "#D8DEE9", -- light fg (white1)
+  base07 = "#ECEFF4", -- bright fg (white3)
+  base08 = "#BF616A", -- red - variables/macros
+  base09 = "#D08770", -- orange - keywords/constants
+  base0A = "#EBCB8B", -- yellow - types
+  base0B = "#A3BE8C", -- green - strings
+  base0C = "#8FBCBB", -- cyan - support
+  base0D = "#88C0D0", -- blue2 - functions
+  base0E = "#B48EAD", -- magenta - keywords alt
+  base0F = "#5E81AC", -- blue0 - special/builtin
+}
+
+-- Override highlights to match original nordic.nvim behavior
+M.highlights = {
+  -- Functions use blue2
+  Function = { fg = M.colors.blue2 },
+  ["@function"] = { fg = M.colors.blue2 },
+  ["@function.builtin"] = { fg = M.colors.blue2 },
+  ["@function.call"] = { fg = M.colors.blue2 },
+  ["@method"] = { fg = M.colors.blue2 },
+  ["@method.call"] = { fg = M.colors.blue2 },
+  ["@function.method.call"] = { fg = M.colors.blue2 },
+
+  -- Function macros use red
+  ["@function.macro"] = { fg = M.colors.red },
+  Macro = { fg = M.colors.red },
+
+  -- Keywords use orange (with bold option in original)
+  Keyword = { fg = M.colors.orange },
+  Statement = { fg = M.colors.orange },
+  Conditional = { fg = M.colors.orange },
+  Repeat = { fg = M.colors.orange },
+  ["@keyword"] = { fg = M.colors.orange },
+  ["@keyword.function"] = { fg = M.colors.orange },
+  ["@keyword.return"] = { fg = M.colors.orange },
+  ["@keyword.operator"] = { fg = M.colors.orange },
+  ["@keyword.conditional"] = { fg = M.colors.orange },
+  ["@keyword.repeat"] = { fg = M.colors.orange },
+  ["@conditional"] = { fg = M.colors.orange },
+  ["@repeat"] = { fg = M.colors.orange },
+
+  -- Types use yellow
+  Type = { fg = M.colors.yellow },
+  ["@type"] = { fg = M.colors.yellow },
+  ["@type.builtin"] = { fg = M.colors.yellow },
+  ["@type.definition"] = { fg = M.colors.yellow },
+  Typedef = { fg = M.colors.yellow },
+
+  -- Type qualifier uses keyword color
+  ["@type.qualifier"] = { fg = M.colors.orange },
+
+  -- Variables use fg
+  ["@variable"] = { fg = M.colors.white },
+  Identifier = { fg = M.colors.white },
+
+  -- Variable builtin uses blue0
+  ["@variable.builtin"] = { fg = M.colors.blue0 },
+
+  -- Variable member (fields)
+  ["@variable.member"] = { fg = M.colors.white },
+  ["@field"] = { fg = M.colors.white },
+  ["@property"] = { fg = M.colors.white },
+
+  -- Parameters use fg with italic
+  ["@parameter"] = { fg = M.colors.white, italic = true },
+  ["@variable.parameter"] = { fg = M.colors.white, italic = true },
+
+  -- Constants use magenta.bright
+  Constant = { fg = M.colors.purple_bright },
+  ["@constant"] = { fg = M.colors.purple_bright },
+  ["@constant.builtin"] = { fg = M.colors.purple_bright },
+  ["@constant.macro"] = { fg = M.colors.purple_bright },
+
+  -- Numbers link to Constant (magenta.bright)
+  Number = { fg = M.colors.purple_bright },
+  Float = { fg = M.colors.purple_bright },
+  Boolean = { fg = M.colors.purple_bright },
+  ["@number"] = { fg = M.colors.purple_bright },
+  ["@float"] = { fg = M.colors.purple_bright },
+  ["@boolean"] = { fg = M.colors.purple_bright },
+
+  -- Strings use green
+  String = { fg = M.colors.green },
+  Character = { fg = M.colors.green },
+  ["@string"] = { fg = M.colors.green },
+  ["@string.documentation"] = { fg = M.colors.green },
+  ["@character"] = { fg = M.colors.green },
+
+  -- String escape/regex use magenta.bright
+  ["@string.escape"] = { fg = M.colors.purple_bright },
+  ["@string.regex"] = { fg = M.colors.purple_bright },
+  ["@string.special"] = { fg = M.colors.yellow },
+
+  -- Operators use fg
+  Operator = { fg = M.colors.white },
+  ["@operator"] = { fg = M.colors.white },
+
+  -- Punctuation
+  ["@punctuation.bracket"] = { fg = M.colors.white },
+  ["@punctuation.delimiter"] = { fg = M.colors.white },
+  ["@punctuation.special"] = { fg = M.colors.blue1 },
+  Delimiter = { fg = M.colors.white },
+
+  -- Special uses blue1
+  Special = { fg = M.colors.blue1 },
+  SpecialChar = { fg = M.colors.blue1 },
+
+  -- Builtins use blue0
+  ["@builtin"] = { fg = M.colors.blue0 },
+
+  -- Constructors
+  ["@constructor"] = { fg = M.colors.yellow },
+
+  -- Include/imports
+  Include = { fg = M.colors.blue1 },
+  ["@include"] = { fg = M.colors.blue1 },
+  ["@keyword.import"] = { fg = M.colors.blue1 },
+
+  -- PreProc
+  PreProc = { fg = M.colors.blue1 },
+  Define = { fg = M.colors.blue1 },
+  ["@keyword.directive"] = { fg = M.colors.blue1 },
+
+  -- Comments use gray5
+  Comment = { fg = M.colors.grey_fg, italic = true },
+  ["@comment"] = { fg = M.colors.grey_fg, italic = true },
+  ["@comment.documentation"] = { fg = M.colors.grey_fg, italic = true },
+
+  -- Comment annotations
+  ["@comment.note"] = { fg = M.colors.hint },
+  ["@comment.error"] = { fg = M.colors.error },
+  ["@comment.warning"] = { fg = M.colors.warn },
+  ["@comment.todo"] = { fg = M.colors.info },
+
+  -- Tags (HTML/JSX)
+  Tag = { fg = M.colors.blue1 },
+  ["@tag"] = { fg = M.colors.blue1 },
+  ["@tag.attribute"] = { fg = M.colors.yellow },
+  ["@tag.delimiter"] = { fg = M.colors.white },
+
+  -- Namespace/module
+  ["@namespace"] = { fg = M.colors.white },
+  ["@module"] = { fg = M.colors.white },
+
+  -- Labels
+  Label = { fg = M.colors.blue1 },
+  ["@label"] = { fg = M.colors.blue1 },
+
+  -- Storage
+  StorageClass = { fg = M.colors.orange },
+  ["@keyword.storage"] = { fg = M.colors.orange },
+  ["@storageclass"] = { fg = M.colors.orange },
+
+  -- Exception
+  Exception = { fg = M.colors.orange },
+  ["@exception"] = { fg = M.colors.orange },
+  ["@keyword.exception"] = { fg = M.colors.orange },
+
+  -- Debug
+  Debug = { fg = M.colors.red },
+  ["@keyword.debug"] = { fg = M.colors.red },
+
+  -- Coroutine keywords use macro color (red)
+  ["@keyword.coroutine"] = { fg = M.colors.red },
+
+  -- Attribute
+  ["@attribute"] = { fg = M.colors.yellow },
+
+  -- C++ property uses cyan
+  ["@property.cpp"] = { fg = M.colors.cyan },
+}
+
+require("xaaha.core.colorscheme").setup_dark(M.colors, M.highlights)
