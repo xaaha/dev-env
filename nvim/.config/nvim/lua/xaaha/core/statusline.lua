86f9a6d859de9696c5428548c21de87d2af755c2
xaaha <pratik.philos@gmail.com>
2026-01-27 14:46:31 -0500

    helpful info

--- nvim/.config/nvim/lua/xaaha/core/statusline.lua
+++ nvim/.config/nvim/lua/xaaha/core/statusline.lua
@@ -10,19 +10,43 @@ local function git_branch()
   return "  " .. branch .. " "
 end
 
+local function file_size()
+  local file = vim.fn.expand("%:p")
+  if file == "" then
+    return ""
+  end
+
+  local size = vim.fn.getfsize(file)
+  if size < 0 then
+    return ""
+  end
+
+  -- human-readable
+  local units = { "B", "KB", "MB", "GB" }
+  local i = 1
+  while size >= 1024 and i < #units do
+    size = size / 1024
+    i = i + 1
+  end
+
+  return string.format(" %.1f%s ", size, units[i])
+end
+
+
+
 local statusline = table.concat({
-  " %f " .. chevron_left, -- File name/path
-  " %r",                  -- Readonly flag
-  "%#GitBranch#",         -- Git highlight start
-  "%*",                   -- Reset highlight
+  " %f " .. " | %{v:lua.git_branch()}" .. chevron_left, -- File name/path
+  " %r",                                                -- Readonly flag
+  "%#GitBranch#",                                       -- Git highlight start
+  "%*",                                                 -- Reset highlight
   "%#GitChanges#",
-  "%{get(b:,'gitsigns_status','')}",
+  " %{get(b:,'gitsigns_status','')}",
   "%*",
   " %m ", -- Modified flag
   "%=",   -- divider
-  chevron_right .. " %{v:lua.git_branch()}",
+  chevron_right .. " %{v:lua.file_size()} | %L Lines ",
 })
 
 _G.git_branch = git_branch
-
+_G.file_size = file_size
 vim.o.statusline = statusline
