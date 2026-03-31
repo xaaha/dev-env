e7108f37f67cc187523a194ded938490f8ce287a
Pratik Thapa <pratikthapa@pratiks-mbp.mynetworksettings.com>
2026-03-31 11:31:46 -0400

    minor

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -3,6 +3,7 @@ local opt = vim.opt -- for conciseness
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
 opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
+opt.laststatus = 3
 
 -- tabs & indentation
 opt.tabstop = 2       -- 2 spaces for tabs (prettier default)
